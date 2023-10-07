3681143eb1d739f583dee7e32ec3c01a2d79e904
joshuaobare <jobare.obare@gmail.com>
2023-10-07 19:18:21 +0300

    tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    "./src/components/*.{js,jsx,ts,tsx}",
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/*.{js,jsx,ts,tsx}",
+    "./index.html"
     
   ],
   theme: {

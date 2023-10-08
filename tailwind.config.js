2396509ea1062ecb7e22c1caca861c9fc63cf85d
joshuaobare <jobare.obare@gmail.com>
2023-10-08 07:34:35 +0300

    another tailwind fix attempt

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  important:true,
   content: [
-    "./src/components/*.{js,jsx,ts,tsx}",
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./src/*.{js,jsx,ts,tsx}",
-    "./index.html"
-    
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-  
 }
 
